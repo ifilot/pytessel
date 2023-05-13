2c9ba405c7420db6275e41167cf574d32f3c9802
ifilot <ivo@ivofilot.nl>
2023-05-13 21:36:40 +0200

    Reorganizing build procedure

--- build_docker_linux_pypi.sh
+++ build_docker_linux_pypi.sh
@@ -6,8 +6,11 @@ rm -vf dist/*.whl wheelhouse/*.whl
 rm -rvf *.egg-info
 
 # set path to root
-ROOT='//d//PROGRAMMING//PYTHON//pyqint'
+ROOT='//d//PROGRAMMING//PYTHON//pytessel'
 IMAGE='pytessel2014'
 
 # run compilation inside Docker
-winpty docker run -i -t -v $ROOT://io -w //io $IMAGE .//docker//docker_run_pypi.sh
+winpty docker run -i -t -v $ROOT://io -w //io $IMAGE .//deploy//docker_setup.sh
+
+# test
+winpty docker run -i -t -v $ROOT://io -w //io $IMAGE .//deploy//docker_test.sh
