3df0e28e8640e85f5bb9a93b963ff15969a677a4
ifilot <ivo@ivofilot.nl>
2023-05-13 21:51:57 +0200

    Removing IO folder

--- deploy/docker_test_github.sh
+++ deploy/docker_test_github.sh
@@ -5,6 +5,6 @@ set -e -u -x
 # Install packages and test
 for PYBIN in /opt/python/cp3[7,8,9]*/bin; do
     "${PYBIN}/python" -m pip install numpy nose
-    "${PYBIN}/pip" install pytessel --no-index -f /io/wheelhouse
+    "${PYBIN}/pip" install pytessel --no-index -f ./wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" --verbose ./tests/*.py)
 done
