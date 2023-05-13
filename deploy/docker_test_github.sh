5e68edc268333821bb386d609ad585236c68118e
ifilot <ivo@ivofilot.nl>
2023-05-13 22:12:37 +0200

    Removing nose from pip

--- deploy/docker_test_github.sh
+++ deploy/docker_test_github.sh
@@ -4,7 +4,7 @@ set -e -u -x
 
 # Install packages and test
 for PYBIN in /opt/python/cp3[7,8,9]*/bin; do
-    "${PYBIN}/python" -m pip install numpy nose
+    "${PYBIN}/python" -m pip install numpy pytest
     "${PYBIN}/pip" install pytessel --no-index -f ./wheelhouse
     ("${PYBIN}/pytest" --verbose ./tests/*.py)
 done
